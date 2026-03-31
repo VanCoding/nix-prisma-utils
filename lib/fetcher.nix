ee7ccbf7e4295e969b6091441f07ebae20605635
Patrik Stutz <patrik.stutz@gmail.com>
2026-03-31 08:58:25 +0200

    force re-fetching when hash changes

--- lib/fetcher.nix
+++ lib/fetcher.nix
@@ -21,7 +21,7 @@ let
   target = if isDarwin then binaryTarget else "${binaryTarget}-openssl-${opensslVersion}";
   toUrl = url: "https://binaries.prisma.sh/all_commits/${version.commit}/${target}/${url}";
   deps =
-    runCommand "prisma-deps-bin"
+    runCommand "prisma-deps-bin-${hash}"
       {
         nativeBuildInputs = [
           curl
